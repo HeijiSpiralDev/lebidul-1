a84f4a0346565b18f43c5494e5d3d87105157fa3
AidanDeshaies <aidan.deshaies@bts-malraux.net>
2026-01-15 11:29:15 +0100

    Liaison de Wagtail à Django

--- LeBidul/urls.py
+++ LeBidul/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail import urls as wagtail_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('manga/', include('mangalib.urls'))
+    path('manga/', include('mangalib.urls')),
+    path('admin/', include (wagtailadmin_urls)),
+    path('',include(wagtail_urls)),
 ]
