73f58e8a98a4a7a38f6adb41376c26379b4576c3
AidanDeshaies <aidan.deshaies@bts-malraux.net>
2026-01-29 15:30:06 +0100

    mise en place templates + setup groupes + setup collections

--- lebidul/urls.py
+++ lebidul/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
 # Debug toolbar
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 
     try:
         import debug_toolbar
